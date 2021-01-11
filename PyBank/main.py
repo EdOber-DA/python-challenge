3df6b64bfa2539c8e8a0cf319ede7a999550571d
ECHO <edward.ober@gmail.com>
2021-01-10 20:21:30 -0500

    Store results after getting printing to file with formatting working

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,20 +9,20 @@ Total_Months = 0                    # Count of the Number of months in the analy
 Total_Profit_Loss = 0               # Sum of the Profit / Loss for each of the months
 
 Greatest_Increase_Amt = 0               # Largest Profit over the entire period of analysis 
-Greatest_Increase_Date = "MMM-YY"   # Date (raw) when the Largest Profit occured during the entire period of analysis
+Greatest_Increase_Date = "MMM-YY"   # Date when the Largest Profit occured during the entire period of analysis
 
 Greatest_Decrease_Amt = 0               # Largest Loss over the entire period of analysis 
-Greatest_Decrease_Date = "MMM-YY"   # Date (raw) when the Largest Loss occured during the entire period of analysis
+Greatest_Decrease_Date = "MMM-YY"   # Date when the Largest Loss occured during the entire period of analysis
 
 
 # Set Path to the data file in the Resources folder
 BankData_csv_path = os.path.join('Resources', 'budget_data.csv')
 
 # Open the data file with Read capabilities
-with open(BankData_csv_path, 'r') as csvfile:
+with open(BankData_csv_path, 'r') as csvfilein:
 
     # Read the data, and since it is a CSV, Split the data on comma delimiters
-    BankData_row = csv.reader(csvfile, delimiter=',')
+    BankData_row = csv.reader(csvfilein, delimiter=',')
 
     # Store the initial header line
     BankData_Header = next(BankData_row)
@@ -52,6 +52,19 @@ print(f"Financial Analysis\n"
     f"----------------------------\n"
     f"Total Months: {Total_Months}\n"
     f"Total: ${Total_Profit_Loss:,}\n"
-    f"Average  Change: ${float(Total_Profit_Loss/Total_Months):,.2f}\n"
+    f"Average Change: ${float(Total_Profit_Loss/Total_Months):,.2f}\n"
     f"Greatest Increase in Profits: {Greatest_Increase_Date} (${Greatest_Increase_Amt:,})\n"
     f"Greatest Decrease in Profits: {Greatest_Decrease_Date} (${Greatest_Decrease_Amt:,})")
+# Set Path to the output file in the analysis folder
+BankDataAnalysis_csv_path = os.path.join('analysis', 'budget_data_analysis.csv')
+
+# Open the data file with write capabilities
+with open(BankDataAnalysis_csv_path, 'w',newline="") as csvfileout:
+     csvwriter = csv.writer(csvfileout, delimiter=',')
+     csvwriter.writerow(["Financial Analysis"])
+     csvwriter.writerow(["----------------------------"])
+     csvwriter.writerow([f"Total Months: {Total_Months}"])
+     csvwriter.writerow([f"Total: ${Total_Profit_Loss:,}"])
+     csvwriter.writerow([f"Average Change: ${float(Total_Profit_Loss/Total_Months):,.2f}"])
+     csvwriter.writerow([f"Greatest Increase in Profits: {Greatest_Increase_Date} (${Greatest_Increase_Amt:,}"])
+     csvwriter.writerow([f"Greatest Decrease in Profits: {Greatest_Decrease_Date} (${Greatest_Decrease_Amt:,})"])
